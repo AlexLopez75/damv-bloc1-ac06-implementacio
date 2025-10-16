fc8e5055e4f446a77b0b6aab5bf46f0ebb07ce4a
AlexLopez75 <alex.lopez.7e9@itb.cat>
2025-10-16 11:57:40 +0200

    Millorada la notació de les constants

--- Program.cs
+++ Program.cs
@@ -5,20 +5,20 @@ namespace SumTwoNumbers
     {
         public static void Main()
         {
-            const string MSG_INPUT1 = "Enter the first integer:";
-            const string MSG_INPUT2 = "Enter the second integer:";
-            const string MSG_RESULT = "The sum is:";
+            const string MsgInput1 = "Enter the first integer:";
+            const string MsgInput2 = "Enter the second integer:";
+            const string MsgResult = "The sum is:";
 
             int num1;
             int num2;
             int sum;
 
-            Console.Write(MSG_INPUT1);
+            Console.Write(MsgInput1);
             num1 = Int32.Parse(Console.ReadLine());
-            Console.Write(MSG_INPUT2);
+            Console.Write(MsgInput2);
             num2 = Int32.Parse(Console.ReadLine());
             sum = num1 + num2;
-            Console.Write(MSG_RESULT + sum);
+            Console.WriteLine(MsgResult + sum);
         }
     }
 }
