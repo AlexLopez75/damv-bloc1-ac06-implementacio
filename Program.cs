407f085d03d80690f48c3db4e10fd260363c9e0f
AlexLopez75 <alex.lopez.7e9@itb.cat>
2025-10-16 12:16:20 +0200

    Afeigt codi exercici 3

--- Program.cs
+++ Program.cs
@@ -1,24 +1,25 @@
 ﻿using System;
-namespace SumTwoNumbers
+namespace RectangleArea
 {
     public class Program
     {
         public static void Main()
         {
-            const string MsgInput1 = "Enter the first integer:";
-            const string MsgInput2 = "Enter the second integer:";
-            const string MsgResult = "The sum is:";
+            // PRECONDITION: The user is prompted to input the width and height of a rectangle.
+            const string MsgInputWidth = "Enter the width of the rectangle:";
+            const string MsgInputHeight = "Enter the height of the rectangle:";
+            const string MsgResult = "The area of the rectangle is:";
 
-            int num1;
-            int num2;
-            int sum;
+            float width;
+            float height;
+            float area;
 
-            Console.Write(MsgInput1);
-            num1 = Int32.Parse(Console.ReadLine());
-            Console.Write(MsgInput2);
-            num2 = Int32.Parse(Console.ReadLine());
-            sum = num1 + num2;
-            Console.WriteLine(MsgResult + sum);
+            Console.Write(MsgInputWidth);
+            width = Int32.Parse(Console.ReadLine());
+            Console.Write(MsgInputHeight);
+            height = Int32.Parse(Console.ReadLine());
+            area = width * height;
+            Console.WriteLine(MsgResult + area);
         }
     }
 }
