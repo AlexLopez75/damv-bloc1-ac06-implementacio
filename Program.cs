d0c231e190ab27b360365b88042726df523c57fb
AlexLopez75 <alex.lopez.7e9@itb.cat>
2025-10-21 12:37:27 +0200

    Afigit codi ExerciseOne

--- Program.cs
+++ Program.cs
@@ -1,11 +1,48 @@
 ﻿using System;
-namespace SumTwoNumbers
+namespace IsNaturalNumber
 {
     public class Program
     {
         public static void Main()
         {
-            Console.WriteLine("Hello World");
+            // PRECONDITION: The algorithm is ready to accept any integer input value.
+            const string MsgInput = "Enter a value to check if it's a natural number:";
+            const string MsgError = "You have to enter a natural number.";
+            const string MsgOk = " is a natural number.";
+            const string MsgKo = " is NOT a natural number.";
+
+            int value;
+            bool isNatural, isInteger;
+
+            Console.WriteLine(MsgInput);
+            isInteger = Int32.TryParse(Console.ReadLine(), out value);
+
+            if (isInteger)
+            {
+                if ((value >= 0) && (value == value))
+                {
+                    isNatural = true;
+                }
+                else
+                {
+                    isNatural = false;
+                }
+
+                if (isNatural)
+                {
+                    Console.WriteLine(MsgOk);
+                }
+                else
+                {
+                    Console.WriteLine(MsgKo);
+
+                }
+            }
+            else
+            {
+                Console.Write(MsgError);
+            }
+
         }
     }
 }
